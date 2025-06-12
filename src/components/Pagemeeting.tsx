019e78cfe335bd250489e0c0deb1d3065a6fef5a
samradhiR <samradhi4320@gmail.com>
2025-06-12 22:39:54 +0530

    c

--- src/components/Pagemeeting.tsx
+++ src/components/Pagemeeting.tsx
@@ -1,16 +1,16 @@
-
-import Navbar from './Navbar'
-import LeftSidebar from './LeftSidebar'
-import Meetingarea from './Meetingarea'
+import Navbar from './Navbar';
+import LeftSidebar from './LeftSidebar';
+import Meetingarea from './Meetingarea';
 
 export default function Pagemeeting() {
   return (
-    <div className='m-3'>
-      <Navbar/>
-      <div className='flex gap-2'>
-        <LeftSidebar/>
-        <Meetingarea/>
+    <div className="m-2 md:m-3">
+      <Navbar />
+      <div className="flex flex-col md:flex-row gap-2">
+        {/* Sidebar should shrink or collapse on mobile if needed */}
+        <LeftSidebar />
+        <Meetingarea />
       </div>
     </div>
-  )
+  );
 }
